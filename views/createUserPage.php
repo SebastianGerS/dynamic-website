81fbf371a7ef1e18123ca92e476a4e4b7cc31526
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2017-11-19 02:13:30 +0100

    HTML turn of autocomplete
    	* set autocomplete="off" to all inputfields in on in the
     	  createUserPage view

--- views/createUserPage.php
+++ views/createUserPage.php
@@ -2,11 +2,11 @@
 <div class="form-group py-3">
     <article id="create-user-form"> 
         <form action="/createUser" method="post">
-            <input class="form-control my-3" name="firstname" type="text" placeholder="Förnamn" required>
-            <input class="form-control my-3"name="surname" type="text" placeholder="Efternamn" required>
-            <input class="form-control my-3"name="email" type="text" placeholder="Epost" required>
-            <input class="form-control my-3"name="username" type="text" placeholder="Användarnamn" required>
-            <input class="form-control my-3"name="password" type="text" placeholder="Lösenord" required>
+            <input class="form-control my-3" name="firstname" type="text" placeholder="Förnamn" required autocomplete="off">
+            <input class="form-control my-3"name="surname" type="text" placeholder="Efternamn" required autocomplete="off">
+            <input class="form-control my-3"name="email" type="text" placeholder="Epost" required autocomplete="off">
+            <input class="form-control my-3"name="username" type="text" placeholder="Användarnamn" required autocomplete="off">
+            <input class="form-control my-3"name="password" type="text" placeholder="Lösenord" required autocomplete="off">
             <button class="btn btn-success" type="submit">Skapa användare</button>
         </form>  
     </article>

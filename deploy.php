bfe37d31545bcd07dfc24abe97ac5f285ec52aa8
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2017-11-27 13:40:06 +0100

    Change multiplex, again

--- deploy.php
+++ deploy.php
@@ -11,7 +11,7 @@ set('repository', 'git@github.com:chas-academy/05-dynamisk-webbplats-php-Sebasti
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', false);
-set('ssh_multiplexing', false);
+set('ssh_multiplexing', native);
 
 // Shared files/dirs between deploys 
 set('shared_files', ['config/dbinfo.json']);
